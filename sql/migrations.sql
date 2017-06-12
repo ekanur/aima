414ea4b4207a01b006bbc49c9906f685a8af5cdb
Eka Nur Ahmad R <ekanur@um.ac.id>
2017-06-12 07:50:40 +0700

    tabel export

---
+++ sql/migrations.sql
@@ -0,0 +1,6 @@
+INSERT INTO "migrations" ("id", "migration", "batch") VALUES (1, '2017_05_23_033522_Standar2', 1);
+INSERT INTO "migrations" ("id", "migration", "batch") VALUES (2, '2017_05_23_035718_Standar3', 1);
+INSERT INTO "migrations" ("id", "migration", "batch") VALUES (3, '2017_05_23_035728_Standar4', 1);
+INSERT INTO "migrations" ("id", "migration", "batch") VALUES (4, '2017_05_23_035739_Standar5', 1);
+INSERT INTO "migrations" ("id", "migration", "batch") VALUES (5, '2017_05_23_035749_Standar6', 1);
+INSERT INTO "migrations" ("id", "migration", "batch") VALUES (6, '2017_05_23_035757_Standar7', 1);
---
+++ sql/standar2s.sql
@@ -0,0 +1,3 @@
+INSERT INTO "standar2s" ("id", "kode", "nilai", "id_jurusan", "created_at", "updated_at", "deleted_at") VALUES (4, '2.1', 4, 1, '2017-6-5 02:08:33', '2017-6-5 02:08:33', NULL);
+INSERT INTO "standar2s" ("id", "kode", "nilai", "id_jurusan", "created_at", "updated_at", "deleted_at") VALUES (5, '2.2', 1, 1, '2017-6-5 02:08:33', '2017-6-5 02:08:33', NULL);
+INSERT INTO "standar2s" ("id", "kode", "nilai", "id_jurusan", "created_at", "updated_at", "deleted_at") VALUES (6, '2.6', 2, 1, '2017-6-5 02:08:33', '2017-6-5 02:08:33', NULL);
